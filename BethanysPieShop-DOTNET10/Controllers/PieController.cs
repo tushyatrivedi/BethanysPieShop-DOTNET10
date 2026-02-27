c57d8df725a268c0c2dfb3494f11708680be9062
TUSHYA TRIVEDI <ttushya@gamil.com>
2026-02-27 13:24:15 +0530

    added skeleton of controller and view for pie list view

---
+++ BethanysPieShop-DOTNET10/Controllers/PieController.cs
@@ -0,0 +1,13 @@
+﻿using BethanysPieShop_DOTNET10.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop_DOTNET10.Controllers
+{
+    public class PieController : Controller
+    {
+        public IActionResult List()
+        {
+            return View(StaticPieData.GetAll());
+        }
+    }
+}
