b4e4a3bf3c5e61dc90e38ec31186c224306a4b6f
TUSHYA TRIVEDI <ttushya@gamil.com>
2026-02-27 12:24:37 +0530

    added models folder, model class Pie and a static data store for it

---
+++ BethanysPieShop-DOTNET10/Models/StaticPieData.cs
@@ -0,0 +1,16 @@
+﻿namespace BethanysPieShop_DOTNET10.Models
+{
+    public class StaticPieData
+    {
+        private static readonly List<Pie> _pies = new List<Pie>()
+    {
+        new Pie { PieId = 1, Name = "Apple Pie", Price = 15.95M, ShortDescription = "Classic apple pie", InStock = true, IsPieOfTheWeek = false },
+        new Pie { PieId = 2, Name = "Cherry Pie", Price = 18.95M, ShortDescription = "Sweet cherry pie", InStock = true, IsPieOfTheWeek = true },
+        new Pie { PieId = 3, Name = "Pumpkin Pie", Price = 12.95M, ShortDescription = "Seasonal favorite", InStock = false, IsPieOfTheWeek = false }
+    };
+
+        public static IEnumerable<Pie> GetAll() => _pies.OrderBy(p=>p.PieId);
+
+        public static Pie? GetById(int id) => _pies.FirstOrDefault(x => x.PieId == id);
+    }
+}
