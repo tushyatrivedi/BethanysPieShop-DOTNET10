b8685717426c541b0a437266add60dfe2109124e
TUSHYA TRIVEDI <ttushya@gamil.com>
2026-02-28 11:08:43 +0530

    added the POST method to handle form submission and basic model validation

--- BethanysPieShop-DOTNET10/Controllers/ContactController.cs
+++ BethanysPieShop-DOTNET10/Controllers/ContactController.cs
@@ -1,10 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BethanysPieShop_DOTNET10.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop_DOTNET10.Controllers
 {
     public class ContactController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(item);
+            }
+            StaticContactData.AddContact(item);
+            TempData["Message"] = "Message Recieved, we'll get back to you shortly!!";
+            return RedirectToAction(nameof(Thanks));
+        }
+
+        [HttpGet]
+        public IActionResult Thanks()
         {
             return View();
         }
--- BethanysPieShop-DOTNET10/Models/Contact.cs
+++ BethanysPieShop-DOTNET10/Models/Contact.cs
@@ -1,9 +1,16 @@
-﻿namespace BethanysPieShop_DOTNET10.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BethanysPieShop_DOTNET10.Models
 {
     public class Contact
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
+
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
         public string? Subject { get; set; }
         public string Message { get; set; } = string.Empty;
